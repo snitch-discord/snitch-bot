82f073faef81fe26f067a7f275dca0db3c3ef464
Emery Tanghanwaye <emerytang@gmail.com>
2025-01-23 19:30:11 -0500

    handler: register: add basic register feedback

--- internal/slashcommand/handler/register.go
+++ internal/slashcommand/handler/register.go
@@ -67,6 +67,16 @@ func handleCreateGroup(ctx context.Context, session *discordgo.Session, interact
 		body, _ := io.ReadAll(response.Body)
 		defer response.Body.Close()
 		slogger.ErrorContext(ctx, "Unexpected Response", "Status", response.StatusCode, "Body", string(body))
+	
+		if err = session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: fmt.Sprintf("Couldn't register group, error: %s", body),
+			},
+		}); err != nil {
+			slogger.ErrorContext(ctx, "Couldn't Write Discord Response", "Error", err)
+		}
+
 		return
 	}
 
